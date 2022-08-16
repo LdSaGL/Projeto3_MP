adfed158f228161b38d950574b5c60c611ab0545
LdSaGL <lucas.sa.licio@gmail.com>
2022-08-16 18:41:54 -0300

    Tabela de decisao (backup e restore)

--- backup.py
+++ backup.py
@@ -1,3 +1,8 @@
+"""
+Este programa utiliza a tabela de decisao apresentada em sala para realizar um backup ou uma
+restauracao
+"""
+
 import os
 import shutil
 
@@ -12,7 +17,7 @@ def arquivos_analisados(argumento):
     """
     parm = []
     if os.path.isfile(f'{argumento}.txt'):
-        with open(f"{argumento}.txt") as file:
+        with open(f"{argumento}.txt", 'r', encoding='utf-8') as file:
             parm = file.readlines()
             parm = [line.rstrip() for line in parm]
     return parm
@@ -23,27 +28,89 @@ def extrator_tempo(lista_arquivos):
     Recebe a lista com os arquivos em analise (retirados do arquivo parm), descobre e
     armazena a ultima data de modificacao tanto na pasta HD quanto na Pendrive.
 
-    :param arquivos: list
+    :param lista_arquivos: list
     :return dict tempos (data de modificacao em 'HD' e 'Pendrive')
     """
     tempos = {}
-    for termo in lista_arquivos:
-        if os.path.isfile(f'HD/{termo}.txt'):
-            hd_file = open(f'HD/{termo}.txt')
-            tempo1 = os.path.getmtime(f'D:/Projetos/Projeto3_MP/HD/{termo}.txt')
-            hd_file.close()
-            tempos[termo] = [tempo1]
+    if len(lista_arquivos) == 1:
+        arquivo = lista_arquivos[0]
+        if os.path.isfile(f'HD/{arquivo}.txt'):
+            tempo1 = os.path.getmtime(f'D:/Projetos/Projeto3_MP/HD/{arquivo}.txt')
+            tempos[arquivo] = [tempo1]
         else:
-            tempos[termo] = ['inexistente']
+            tempos[arquivo] = ['inexistente']
 
 
-        if os.path.isfile(f'Pendrive/{termo}.txt'):
-            pendrive_file = open(f'Pendrive/{termo}.txt')
-            tempo2 = os.path.getmtime(f'D:/Projetos/Projeto3_MP/Pendrive/{termo}.txt')
-            pendrive_file.close()
-            tempos[termo].append(tempo2)
+        if os.path.isfile(f'Pendrive/{arquivo}.txt'):
+            tempo2 = os.path.getmtime(f'D:/Projetos/Projeto3_MP/Pendrive/{arquivo}.txt')
+            tempos[arquivo].append(tempo2)
         else:
-            tempos[termo].append('inexistente')
+            tempos[arquivo].append('inexistente')
+
+    else:
+        for termo in lista_arquivos:
+            if os.path.isfile(f'HD/{termo}.txt'):
+                tempo1 = os.path.getmtime(f'D:/Projetos/Projeto3_MP/HD/{termo}.txt')
+                tempos[termo] = [tempo1]
+            else:
+                tempos[termo] = ['inexistente']
+
+
+            if os.path.isfile(f'Pendrive/{termo}.txt'):
+                tempo2 = os.path.getmtime(f'D:/Projetos/Projeto3_MP/Pendrive/{termo}.txt')
+                tempos[termo].append(tempo2)
+            else:
+                tempos[termo].append('inexistente')
     return tempos
 
-print(extrator_tempo(('arquivo1', 'arquivo2', 'arquivo3')))
+
+def backup(lista_arquivos):
+    """
+    E chamada quando a primeira linha do backup_parm for: 'backup'
+    Recebe a lista com os arquivos em analise (retirados do arquivo parm), chama a funcao
+    'extrator_tempo', executa a parte da tabela de decisao relacionada ao backup
+    (HD para Pendrive) e retorna se deve ser feito o backup.
+
+    :param lista_arquivos: list
+    :return list tarefas mensagem de erro ou solicitacao de backup
+    """
+    tempos = extrator_tempo(lista_arquivos)
+    tarefa = []
+    for i in tempos:
+        if tempos[i][0] == 'inexistente' and tempos[i][1] == 'inexistente':
+            tarefa.append('Erro: arquivos nao existem em nenhum dos diretorios.')
+        elif tempos[i][1] == 'inexistente':
+            tarefa.append(f'Fazer backup do: {i}')
+        elif tempos[i][0] != 'inexistente':
+            if tempos[i][0] > tempos[i][1]:
+                tarefa.append(f'Fazer backup do: {i}')
+            elif tempos[i][0] < tempos[i][1]:
+                tarefa.append('Erro: o arquivo do Pendrive mais recente do que o do HD.')
+    return tarefa
+
+
+def restore(lista_arquivos):
+    """
+    E chamada quando a primeira linha do backup_parm for: 'restore'
+    Recebe a lista com os arquivos em analise (retirados do arquivo parm), chama a funcao
+    'extrator_tempo', executa a parte da tabela de decisao relacionada a restauracao
+    (Pendrive para HD) e retorna se deve ser feita a restauracao.
+
+
+    :param lista_arquivos: list
+    :return list tarefas mensagem de erro ou solicitacao de reatauracao
+    """
+    tempos = extrator_tempo(lista_arquivos)
+    tarefa = []
+    for j in tempos:
+        if tempos[j][0] == 'inexistente' and tempos[j][1] == 'inexistente':
+            tarefa.append('Erro: arquivos nao existem em nenhum dos diretorios.')
+        elif tempos[j][1] == 'inexistente':
+            tarefa.append('Erro: arquivo presente apenas no HD.')
+        elif tempos[j][0] == 'inexistente':
+            tarefa.append(f'Fazer restauracao do: {j}')
+        elif tempos[j][0] > tempos[j][1]:
+            tarefa.append('Erro: arquivo do HD mais recente do que o do Pendrive.')
+        elif tempos[j][0] < tempos[j][1]:
+            tarefa.append(f'Fazer restauracao do: {j}')
+    return tarefa
--- test_backup.py
+++ test_backup.py
@@ -1,4 +1,3 @@
-from msilib.schema import Error
 import backup
 
 # 1 - Testa se a funcao consegue extrair efetivamente todas as informacoes do arquivo
@@ -14,10 +13,52 @@ def testeParm2():
 # 2 - Testa se o data de modificacao obtida por meio da funcao condiz com a verdadeira
 
 def testeTempo1():
-    assert backup.extrator_tempo(('arquivo1', 'arquivo2', 'arquivo3')) == {'arquivo1': [1660614617.2689965, 1660614629.3149662], 'arquivo2': [1660616812.60954, 1660616807.5729384], 'arquivo3': ['inexistente', 1660616818.9820714]}
+    assert backup.extrator_tempo(['arquivo1', 'arquivo2', 'arquivo3']) == {'arquivo1': [1660614617.2689965, 1660614629.3149662], 'arquivo2': [1660616812.60954, 1660616807.5729384], 'arquivo3': ['inexistente', 1660616818.9820714]}
 
 def testeTempo2():
-    assert backup.extrator_tempo(('arq', 'arquivo1', 'sistemawin')) == {'arq': ['inexistente', 'inexistente'], 'arquivo1': [1660614617.2689965, 1660614629.3149662], 'sistemawin': ['inexistente', 'inexistente']}
+    assert backup.extrator_tempo(['arq', 'arquivo1', 'sistemawin']) == {'arq': ['inexistente', 'inexistente'], 'arquivo1': [1660614617.2689965, 1660614629.3149662], 'sistemawin': ['inexistente', 'inexistente']}
 
 def testeTempo3():
-    assert backup.extrator_tempo(()) == {}
+    assert backup.extrator_tempo([]) == {}
+
+
+# 3 - Testa se a funcao backup esta executando conforme tabela de decisao
+
+def testeBackup1():
+    assert backup.backup(['arquivo9']) == ['Erro: arquivos nao existem em nenhum dos diretorios.']
+
+def testeBackup2():
+    assert backup.backup(['arquivo4']) == ['Fazer backup do: arquivo4']
+
+def testeBackup3():
+    assert backup.backup(['arquivo1']) == ['Erro: o arquivo do Pendrive mais recente do que o do HD.']
+
+def testeBackup4():
+    assert backup.backup(['arquivo2']) == ['Fazer backup do: arquivo2']
+
+def testeBackup5():
+    assert backup.backup([]) == []
+
+def testeBackup6():
+    assert backup.backup(['arquivo3']) == []
+
+
+# 4 - Testa se a funcao restauracao esta executando conforme tabela de decisao
+
+def testeRestauracao1():
+    assert backup.restore(['arquivox']) == ['Erro: arquivos nao existem em nenhum dos diretorios.']
+
+def testeRestauracao2():
+    assert backup.restore(['arquivo4']) == ['Erro: arquivo presente apenas no HD.']
+
+def testeRestauracao3():
+    assert backup.restore(['arquivo3']) == ['Fazer restauracao do: arquivo3']
+
+def testeRestauracao4():
+    assert backup.restore(['arquivo1']) == ['Fazer restauracao do: arquivo1']
+
+def testeRestauracao5():
+    assert backup.restore(['arquivo2']) == ['Erro: arquivo do HD mais recente do que o do Pendrive.']
+
+def testeRestauracao6():
+    assert backup.restore([]) == []
